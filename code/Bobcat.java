3a65c594d954cdb1d7ae4b12a9e9c10777e54625
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2025-01-08 22:30:44 +0700

    Support --deployment-descriptor

--- code/Bobcat.java
+++ code/Bobcat.java
@@ -3,7 +3,11 @@
  * 
  * Example of usage:
  * 
- * java Bobcat --home web --port 7300
+ * java Bobcat 
+ *     --home web 
+ *     --port 7300
+ *     --deployment-descriptor web.xml
+ *     --verbose
  *
  */
 import java.io.File;
@@ -22,6 +26,7 @@ public class Bobcat {
 		String home = "web";
 		String working = "working";
 		boolean verbose = false;
+		String descriptor = null; 
 		
 		for (int i = 0; i < data.length; i++) {
 			if ("--port".equals(data[i])) {
@@ -35,29 +40,37 @@ public class Bobcat {
 			if ("--verbose".equals(data[i])) {
 				verbose = true;
 			}
+			if ("--deployment-descriptor".equals(data[i])) {
+				i++;
+				descriptor = data[i];
+			}
+		}
+		if (descriptor == null) { 
+			descriptor = home + "/WEB-INF/web.xml"; 
 		}
 
 		// LogManager.getLogManager().reset();
-
 		/*
 		Logger logger = Logger.getGlobal();
 		System.out.println(logger);
 		logger.setLevel(Level.OFF);
 		*/
 		
-		try {	
-			Tomcat tomcat = new Tomcat();
+
+		try {
+			var tomcat = new Tomcat();
 			tomcat.setPort(port);
 			tomcat.setBaseDir(working);
 			tomcat.setSilent(!verbose);
-			tomcat.setAddDefaultWebXmlToWebapp(true);
-			Connector connector = tomcat.getConnector();   // Mandatory
-			File file = new File(home);
-			Context c = tomcat.addWebapp("", file.getAbsolutePath());
-			// c.setAltDDName(file.getAbsolutePath() + "/../web.xml");
 			
+			File file = new File(home);
+			var context = tomcat.addWebapp("", file.getAbsolutePath());
+			context.setAltDDName(descriptor);
+			
+			var connector = tomcat.getConnector(); // Mandatory
 			tomcat.start();
 			tomcat.getServer().await();
+			
 		} catch (Exception e) {
 			System.out.println("ERROR " + e);
 		}
